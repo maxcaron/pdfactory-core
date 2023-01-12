78e86d08cb0f24e5b06fd859e4485af92e0056af
Maxime Caron <maxime@bloc.solutions>
2023-01-12 09:56:38 -0500

    rename compileRenderingFunctions to renderingFunctions

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,79 +1,80 @@
-import * as url from "url";
-import path from "path";
-import typescript from "@rollup/plugin-typescript";
-import terser from "@rollup/plugin-terser";
-import del from "rollup-plugin-delete";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import dotenv from "dotenv";
-import { BrowserFetcher } from "puppeteer";
-import { defineConfig } from "rollup";
+import * as url from 'url';
+import path from 'path';
+import { readFileSync } from 'fs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import del from 'rollup-plugin-delete';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import dotenv from 'dotenv';
+import { BrowserFetcher } from 'puppeteer';
+import { defineConfig } from 'rollup';
 
-import pkg from "../package.json" assert { type: "json" };
+const pkg = JSON.parse(readFileSync('./package.json'));
 
 const downloadPath = `${process.cwd()}/dist/.local-chromium`;
 
 const createBrowserFetcher = () => {
   return new BrowserFetcher({
     path: downloadPath,
-    localRevisions: ["1045629"],
+    localRevisions: ['1045629']
   });
 };
 
 dotenv.config({
-  path: process.cwd() + `/config/.env.${process.env.DEV ? "dev" : "procution"}`,
+  path: process.cwd() + `/config/.env.${process.env.DEV ? 'dev' : 'procution'}`
 });
 
-const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const devMode = process.env.NODE_ENV === "development";
+const devMode = process.env.NODE_ENV === 'development';
 
 const browserFetcher = createBrowserFetcher();
-const revisionInfo = await browserFetcher.download("1045629");
+const revisionInfo = await browserFetcher.download('1045629');
 
 if (!revisionInfo?.executablePath) {
-  throw new Error("Could not find executable path for browser revision");
+  throw new Error('Could not find executable path for browser revision');
 }
 
 process.env.downloadPath = revisionInfo.executablePath;
 
 export default defineConfig([
   {
-    input: "./src/pdfactory.ts",
+    input: './src/pdfactory.ts',
     output: [
       ...(!devMode
         ? [
-            {
-              file: `dist/pdfactory.umd.cjs`,
-              format: "cjs",
-              inlineDynamicImports: true,
-            },
-          ]
+          {
+            file: 'dist/pdfactory.umd.cjs',
+            format: 'cjs',
+            inlineDynamicImports: true
+          }
+        ]
         : []),
       ...(devMode
         ? [
-            {
-              file: "build/bundle.js",
-              format: "es",
-            },
-          ]
-        : []),
+          {
+            file: 'build/bundle.js',
+            format: 'es'
+          }
+        ]
+        : [])
     ],
     external: Object.keys(pkg.dependencies),
     plugins: [
       del({
         targets: [
-          ...(!devMode ? ["dist/**/*"] : []),
-          ...(devMode ? ["build/**/*"] : []),
-        ],
+          ...(!devMode ? ['dist/**/*'] : []),
+          ...(devMode ? ['build/**/*'] : [])
+        ]
       }),
       ...(!devMode ? [terser()] : []),
       typescript({
         tsconfig: path.join(
           dirname,
-          `../tsconfig.json`
-        ),
+          '../tsconfig.json'
+        )
       }),
-      nodeResolve(),
-    ],
-  },
+      nodeResolve()
+    ]
+  }
 ]);
