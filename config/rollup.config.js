113f3e4b211958fb8b6e4cfcc1039a136c5c6545
Maxime Caron <maxime@bloc.solutions>
2023-01-12 11:04:41 -0500

    remove .local-chromium from dist

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -3,7 +3,6 @@ import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import del from 'rollup-plugin-delete';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { BrowserFetcher } from 'puppeteer';
 import { defineConfig } from 'rollup';
 
 import dotenv from 'dotenv';
@@ -17,24 +16,6 @@ const pkg = JSON.parse(readFileSync('./package.json'));
 
 const tsConfig = JSON.parse(readFileSync('./tsconfig.json'));
 tsConfig.compilerOptions.outDir = devMode ? './build' : './dist';
-
-const downloadPath = `${process.cwd()}/dist/.local-chromium`;
-
-const createBrowserFetcher = () => {
-  return new BrowserFetcher({
-    path: downloadPath,
-    localRevisions: ['1045629']
-  });
-};
-
-const browserFetcher = createBrowserFetcher();
-const revisionInfo = await browserFetcher.download('1045629');
-
-if (!revisionInfo?.executablePath) {
-  throw new Error('Could not find executable path for browser revision');
-}
-
-process.env.downloadPath = revisionInfo.executablePath;
 
 export default defineConfig([
   {
